891545a050d51ab4005e5f4a8ff64eea8451d951
Pravallika Jaggavarapu <pravalika.jr17@gmail.com>
2020-07-20 19:22:14 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { MatTableDataSource, MatPaginator } from "@angular/material";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import { throwError as observableThrowError, Observable, Subject } from 'rxjs';
+import {headersToString} from "selenium-webdriver/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  @ViewChild('paginator', {static: false}) paginator: MatPaginator;
+  displayedColumns: string[] = ['numbers'];
+  dataSource;
+  paginatorForm: FormGroup;
+
+  constructor(private http: HttpClient,
+  private fb: FormBuilder){
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.paginatorForm = this.fb.group({
+      number: ['']
+    })
+  }
+  fetchData() {
+    this.http.post<any>('http://localhost:8080/permutations', this.paginatorForm.get('number').value).subscribe( res=> {
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      setTimeout(() => this.dataSource.paginator = this.paginator);
+    })
+  }
+
+  checkForAlphaNumeric(event: any) {
+    return (event.charCode == 13 || (event.charCode >= 48 && event.charCode <= 57)
+      || event.charCode == 13 || event.charCode === 32 || (event.charCode >= 65 &&
+        event.charCode <= 90) || (event.charCode >= 97 && event.charCode <= 122));
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material";
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material';
+import { MatButtonModule, MatCardModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
